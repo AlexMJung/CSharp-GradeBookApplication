24ed968a6016a2c14d160bf098bed67a950735da
Alexander Jung <Alexandermjung89@gmail.com>
2018-03-27 11:53:54 -0500

    added override for RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,6 +11,26 @@ namespace GradeBook.GradeBooks
 			{
 				Type = GradeBookType.Ranked;
 			}
-		
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if (Students.Count < 5)
+			{
+				throw new InvalidOperationException();
+			}
+			else
+			{
+				if (averageGrade >= 90)
+					return 'A';
+				else if (averageGrade >= 80)
+					return 'B';
+				else if (averageGrade >= 70)
+					return 'C';
+				else if (averageGrade >= 60)
+					return 'D';
+				else
+					return 'F';
+			}
+		}
 	}
 }
