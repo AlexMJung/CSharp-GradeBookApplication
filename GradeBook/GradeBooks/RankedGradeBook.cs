ee6c13aec007c6be6e66ee095b91688fde5493c8
Alexander Jung <Alexandermjung89@gmail.com>
2018-03-27 13:10:53 -0500

    try 2 at b grade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,6 +12,8 @@ namespace GradeBook.GradeBooks
 				Type = GradeBookType.Ranked;
 			}
 
+		double highGrade = 0;
+
 		public override char GetLetterGrade(double averageGrade)
 		{
 			if (Students.Count < 5)
@@ -20,17 +22,18 @@ namespace GradeBook.GradeBooks
 			}
 			else
 			{
-				if (averageGrade >= 90)
+				if (averageGrade >= 80)
 					return 'A';
-				else if (averageGrade >= 80)
-					return 'B';
-				else if (averageGrade >= 70)
-					return 'C';
 				else if (averageGrade >= 60)
+					return 'B';
+				else if (averageGrade >= 40)
+					return 'C';
+				else if (averageGrade >= 20)
 					return 'D';
 				else
 					return 'F';
 			}
+
 		}
 	}
 }
